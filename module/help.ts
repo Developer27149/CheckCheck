ed787661dbc39d0ce4c766920a470c62844852eb
Developer27149 <564151947@qq.com>
2023-10-17 23:50:02 +0800

    init

--- module/help.ts
+++
@@ -1,8 +0,0 @@
-export const getHeaderFromRequestHeaders = (
-  requestHeaders: chrome.webRequest.HttpHeader[]
-) => {
-  return requestHeaders.reduce((acc, cur) => {
-    acc[cur.name] = cur.value
-    return acc
-  }, {} as Record<string, string>)
-}
